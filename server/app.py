23798a37cd5760ec130da375ae193c94edcb70ed
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-04-14 14:46:50 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,20 +1,13 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
-
 from models import db, Article, User
-
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
 migrate = Migrate(app, db)
-
 db.init_app(app)
-
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
@@ -23,12 +16,22 @@ def clear_session():
 @app.route('/articles')
 def index_articles():
 
-    pass
+   
+    articles = Article.query.all()
+    articles_dict = [article.to_dict() for article in articles]
+    return make_response(jsonify(articles_dict), 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+   
+    article = Article.query.filter_by(id=id).first()
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+    if session['page_views'] <= 3:
+        return make_response(jsonify(article.to_dict()), 200)
+    else:
+        return make_response(jsonify({'message': 'Maximum pageview limit reached'}), 401)
 
 if __name__ == '__main__':
     app.run(port=5555)
